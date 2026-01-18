faac7cd72718bc0654fa6943890b9b403a73172f
RubyScarlet7255 <scarlet_7255@outlook.com>
2026-01-18 16:55:16 +0800

    统计学习方法绪论

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -106,14 +106,17 @@ export default defineConfig({
             email: "scarlet_7255@outlook.com",
             domain: "\u7AD9\u70B9\u57DF\u540D",
             describe: "\u7F51\u7AD9\u7B80\u4ECB\u3002",
+            avatar: "/pics/misc/avatar.jpg",
             ruleText: "\u6682\u4E0D\u63A5\u53D7\u4E2A\u4EBA\u535A\u5BA2\u4EE5\u5916\u7684\u53CB\u94FE\u7533\u8BF7\uFF0C\u786E\u4FDD\u60A8\u7684\u7F51\u7AD9\u5185\u5BB9\u79EF\u6781\u5411\u4E0A\uFF0C\u6587\u7AE0\u81F3\u5C1130\u7BC7\uFF0C\u539F\u521B70%\u4EE5\u4E0A\uFF0C\u90E8\u7F72HTTPS\u3002"
+          
           },
           banner: {
             type: "img",
             bannerTitle: "",
             bannerText: "欢迎来到我的小世界",
             position: "top",
-            fit: "cover"
+            fit: "cover",
+            bgurl: "/pics/misc/banner.jpg",
           },
           copyrightYear: void 0,
           liveTime: {
